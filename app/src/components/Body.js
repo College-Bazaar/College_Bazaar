2ec85d3e0e937a4eb22295acc697bef264a0add7
Rajesh Panjiyar <rajeshpanjiyar12@gmail.com>
2022-06-13 15:31:49 +0530

    added the website template

---
+++ app/src/components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <>
+    <div className="container text-center">
+       <h1>This is body</h1>
+    </div>
+    </>
+  )
+}
---
+++ app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+   <>
+   <div className="header text-center bg-primary text-white py-3">
+      This is Header
+   </div>
+   </>
+  )
+}
